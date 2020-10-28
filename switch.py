df2d44102395a9e4f737c1bbdace082d7f1e5b23
Bhadrika Evandito Atmomintarso <42692320+vitoatmo@users.noreply.github.com>
2020-10-28 12:23:22 +0700

    done

--- switch.py
+++
@@ -1,9 +0,0 @@
-def print_case(value):
-    if value == 1:
-        print("First case")
-    elif value == 2:
-        print("Second case")
-    else:
-        print("Didn't match a case")
-
-print_case(1)
